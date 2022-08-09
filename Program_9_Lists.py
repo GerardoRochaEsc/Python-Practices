f4b194827c5a0f68b3a97f1ea3e780abe7d49b24
Gera Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2022-08-09 00:19:15 -0500

    Delete Program_9_Lists.py

--- Program_9_Lists.py
+++
@@ -1,7 +0,0 @@
-#color de la posición 3
-colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
-print("El color de la posicion 3 es: "+ colores[3])
-print("El color: "+colores[0]+" esta en la posicion 0,\nmientras que el color: "+colores[7]+" esta en la posicion 7")
-#crea una lista
-lista=["tres","dos","cinco","cuatro","uno"]
-print(lista)
