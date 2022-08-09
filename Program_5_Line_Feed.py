c3994c95de081b2c604ec698fe6598ac2e588ab4
Gera Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2022-08-09 00:18:51 -0500

    Delete Program_5_Line_Feed.py

--- Program_5_Line_Feed.py
+++
@@ -1,2 +0,0 @@
-#Escribe el contenido (strings) de todos estos print() en uno solo con un string.
-print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.\n-HTML.\n-Ensamblador.")
