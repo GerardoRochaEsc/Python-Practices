9f428582195bd400994f423c81ff2868414525d6
Gera Rocha <101081681+GerardoRochaEsc@users.noreply.github.com>
2022-08-09 00:11:26 -0500

    Create          Program_10_Negative_Positions.py

---
+++ Program_10_Negative_Positions.py
@@ -0,0 +1,8 @@
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista.
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores[-1])
+print(colores[-7])
+print(colores[-5])
+print(colores[-2])
+print(colores[-10])
+
